74b4c34fb622b429db995a67955d791b2092fa7f
Gary Anderson <asymptoticallystable@gmail.com>
2013-07-01 13:13:26 -0400

    add chris's model

--- src/mods/genMatrices.m
+++ src/mods/genMatrices.m
@@ -2,8 +2,13 @@ thePwd=pwd;
 mExchangePath=[thePwd '/matlabFileExchange'];
 addpath(SPSolvePath);
 addpath(mExchangePath);
+addpath(thePwd);
 'gen firmvalue matrices'
 pushd './modelez/firmvalue'
 genInsAndOuts('firmvalue')
+popd
+'gen chrisDavidMatt matrices'
+pushd './modelez/chrisDavidMatt'
+genInsAndOuts('modelv1')
 popd
 
