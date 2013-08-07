d5d58737ee19615bd358f321a8a12c0a27fbc1fd
Gary Anderson <asymptoticallystable@gmail.com>
2013-08-07 17:38:15 -0400

    reconciled Will's and my code

--- src/main/cfiles/parserwrapper.c
+++
@@ -1,7 +0,0 @@
-int parserwrapper(int param_,int *np, char modname, int neq, int nlag, int nlead, char eqname_, int eqtype_, int endog_, int delay_, int vtype_ ) {
-
-  //antulio.mod_AMA_data(param_, np, modname, neq, nlag, nlead, eqname_, eqtype_, endog_, delay_, vtype_);
-  //antulio.mod_AMA_matrices() //need to do something about inputs?
-
-  return(0);
-}
